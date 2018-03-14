41727d7a9c82f9999b1b301d2d7e24f8d6206b0c
S00173244 <S00173244@mail.itsligo.ie>
2018-03-14 10:59:54 +0000

    Updated.

--- EasterAssignment/EasterAssignment/Classes/Scene.cs
+++ EasterAssignment/EasterAssignment/Classes/Scene.cs
@@ -9,5 +9,23 @@ namespace EasterAssignment.Classes
      public class Scene
     {
         private string backgroundKey;
+        private IList<string> s;
+        public Scene(string keyForBackground)
+        {
+            backgroundKey = keyForBackground;
+        }
+
+        public string BackgroundKey
+        {
+            get
+            {
+                return backgroundKey;
+            }
+
+            set
+            {
+                backgroundKey = value;
+            }
+        }
     }
 }
---
+++ EasterAssignment/EasterAssignment/Classes/SceneManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasterAssignment.Classes
+{
+    public class SceneManager
+    {
+        private Scene activeScene;
+        private Scene previousScene;
+
+        public Scene ActiveScene
+        {
+            get
+            {
+                return activeScene;
+            }
+
+            set
+            {
+                activeScene = value;
+            }
+        }
+
+        public Scene PreviousScene
+        {
+            get
+            {
+                return previousScene;
+            }
+
+            set
+            {
+                previousScene = value;
+            }
+        }
+    }
+}
