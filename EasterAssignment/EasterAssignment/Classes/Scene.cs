556aee4c519829f2dc4ae9279593021390533bcf
S00173244 <S00173244@mail.itsligo.ie>
2018-03-14 09:14:47 +0000

    Initial Commit

---
+++ EasterAssignment/EasterAssignment/Classes/Scene.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasterAssignment.Classes
+{
+     public class Scene
+    {
+        private string backgroundKey;
+    }
+}
